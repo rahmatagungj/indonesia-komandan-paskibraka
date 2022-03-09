fa23a9e63dfd5491569ed56be29658244432b9ef
MainLah <rendrakusumab@gmail.com>
2022-03-09 18:58:44 +0700

    solution

---
+++ solutions/kusuma/komandanPaskibraka.js
@@ -0,0 +1,27 @@
+class komandanPaskibraka {
+  constructor(name, member = []) {
+    this.name = name;
+    this.member = member;
+  }
+
+  findAndCountPerson(firstPerson, lastPerson) {
+    if (!firstPerson || !lastPerson) return;
+    const tempArray = this.member.flat();
+    const indexFirst = tempArray.indexOf(firstPerson);
+    const indexLast = tempArray.indexOf(lastPerson);
+    const listOfPerson = [];
+    tempArray.forEach((nama, index) => {
+      if (index > indexFirst && index < indexLast) {
+        listOfPerson.push(nama);
+      }
+    });
+    console.log(tempArray, "ini member");
+    return {
+      totalPerson: listOfPerson.length,
+      listOfPerson: listOfPerson,
+    };
+  }
+}
+
+// dont change the code below
+exports.komandanPaskibraka = komandanPaskibraka;
